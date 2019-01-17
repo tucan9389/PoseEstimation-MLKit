936d0bf41a7e0b73a405b446bf38bafcc8d95337
tucan9389 <tucan.dev@gmail.com>
2019-01-17 14:43:23 +0900

    create the PoseEstimation-MLKit project

---
+++ PoseEstimation-MLKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoseEstimation-MLKit
+//
+//  Created by GwakDoyoung on 17/01/2019.
+//  Copyright © 2019 tucan9389. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
