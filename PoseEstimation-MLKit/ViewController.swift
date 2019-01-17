edf67714d601087a7c830a6442542b4989898011
tucan9389 <tucan.dev@gmail.com>
2019-01-17 15:09:36 +0900

    setup the project for camera

--- PoseEstimation-MLKit/ViewController.swift
+++ PoseEstimation-MLKit/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import CoreMedia
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - UI Properties
+    @IBOutlet weak var videoPreview: UIView!
+    
+    // MARK: - AV Property
+    var videoCapture: VideoCapture!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 카메라 세팅
+        setUpCamera()
+    }
+    
+    // MARK: - SetUp Video
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 30
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // UI에 비디오 미리보기 뷰 넣기
+                if let previewLayer = self.videoCapture.previewLayer {
+                    self.videoPreview.layer.addSublayer(previewLayer)
+                    self.resizePreviewLayer()
+                }
+                
+                // 초기설정이 끝나면 라이브 비디오를 시작할 수 있음
+                self.videoCapture.start()
+            }
+        }
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        resizePreviewLayer()
+    }
+    
+    func resizePreviewLayer() {
+        videoCapture.previewLayer?.frame = videoPreview.bounds
+    }
+}
 
+// MARK: - VideoCaptureDelegate
+extension ViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?, timestamp: CMTime) {
+        // 카메라에서 캡쳐된 화면은 pixelBuffer에 담김.
+        // Vision 프레임워크에서는 이미지 대신 pixelBuffer를 바로 사용 가능
+        if let pixelBuffer = pixelBuffer {
+            // start of measure
+            //self.👨‍🔧.🎬👏()
+            
+            // predict!
+            //self.predictUsingVision(pixelBuffer: pixelBuffer)
+        }
+    }
 }
 
